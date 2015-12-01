816850cbb72545e4d8d6a5f732e1ad2f68921eb3
Michael van Bale <mvanbale@gmail.com>
2015-12-02 00:11:39 +0100

    improved console output and cleaned up code

--- day 1/processor.js
+++ day 1/processor.js
@@ -1,32 +1,39 @@
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
+    console.log('Usage:processor.js + inputfile');
+    process.exit(1);
 }
 // Read the file and print its contents.
-var fs = require('fs')
-  , filename = process.argv[2];
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
- findFloor(data);
+var fs = require('fs'),
+    filename = process.argv[2];
+fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) throw err;
+    var results = findFloor(data);
+    console.log("Eventual floornumber: " + results.floor + "\n" +
+        "First Character to go below zero: " + results.basementCharacter);
+
 });
 
-function findFloor(input){
-	var floor = 0;
-	var characterCount = 0;
-	var found = false;
-	for (var i = 0, len = input.length; i < len; i++) {
-		characterCount++;
-		
-      if(input[i] == "("){floor++;};
-	  if (input[i]==")") {
-		  floor--;
-	  }
-	  if(floor<0 &&!found){
-		  found = !found;
-		  console.log("Character first to be lower than 0:"+characterCount) 
-		  }
-}
-console.log(floor);
-	
+function findFloor(input) {
+    var floor = 0;
+    var characterCount = 0;
+    var found = false;
+    for (var i = 0, len = input.length; i < len; i++) {
+        characterCount++;
+        if (input[i] == "(") {
+            floor++;
+        }
+        if (input[i] == ")") {
+            floor--;
+        }
+        if (floor < 0 && !found) {
+            found = !found; //flag so it only runs once
+            var basementCharacter = characterCount;
+        }
+    }
+    return {
+        floor: floor,
+        basementCharacter: basementCharacter
+    };
+
 }
