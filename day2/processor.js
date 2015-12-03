82f1d4380770022abc3fe1f4a8febd08b137d09e
Michael van Bale <mvanbale@gmail.com>
2015-12-03 01:54:12 +0100

    cleaned up code

--- day2/processor.js
+++ day2/processor.js
@@ -14,19 +14,16 @@ fs.readFile(filename, 'utf8', function (err, data) {
 
 function findSurface(input) {
     var splitInput = input.split("\n");
-        splitInput.splice(-1,1)
+    splitInput.splice(-1, 1)
     var totalSquareFeet = 0;
     var totalRibbonLength = 0;
     for (var i = 0, len = splitInput.length; i < len; i++) {
-        var sides = splitInput[i].split('x');
+        var sides = splitInput[i].split('x').sort(sortNumber);
         var height = sides[0]
         var width = sides[1];
         var length = sides[2]
-
-        var sortedSides = sides.sort(sortNumber);
-
-        totalSquareFeet = totalSquareFeet + (((2 * length) * width) + ((2 * width) * height) + ((2 * height) * length) + (sortedSides[0] * sortedSides[1]));
-        totalRibbonLength = Number(totalRibbonLength) + calculateRibbon(sortedSides);
+        totalSquareFeet = totalSquareFeet + (((2 * length) * width) + ((2 * width) * height) + ((2 * height) * length) + (height * width));
+        totalRibbonLength = Number(totalRibbonLength) + calculateRibbon(sides);
         console.log("Square feet of wrapping paper: " + totalSquareFeet);
         console.log("Feet of ribbon required: " + totalRibbonLength);
     }
@@ -34,11 +31,8 @@ function findSurface(input) {
 
 
 function calculateRibbon(sides) {
-
-sides = sides.map(Number);
-
-    return sides[0] + sides[0] + sides[1] + sides[1] +( sides[0] * sides[1] * sides[2]);
-
+    sides = sides.map(Number);
+    return sides[0] + sides[0] + sides[1] + sides[1] + (sides[0] * sides[1] * sides[2]);
 }
 
 function sortNumber(a, b) {
