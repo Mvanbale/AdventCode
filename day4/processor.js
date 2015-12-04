f516c739d660d4f6d9bc99349f1e4dec02280c4d
Michael van Bale <mvanbale@gmail.com>
2015-12-04 12:01:25 +0100

    removed comments

--- day4/processor.js
+++ day4/processor.js
@@ -1,15 +1,8 @@
-
 var md5 = require('md5');
-
-
 var n = 0;
-    while (true) {
-        // console.log(md5(buf + n));
-        
+    while (true) {        
         var generatedHash = md5("ckczppom"+n);
-        // console.log(generatedHash.slice(0,5))
-        // console.log(generatedHash);
-        if(generatedHash.slice(0,8) == "00000000"){
+        if(generatedHash.slice(0,5) == "00000"){
             console.log("ckczppom"+n);
             console.log(generatedHash); break;}
         n++;
